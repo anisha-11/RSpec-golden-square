02e58d0e19029cbcd22a6afd0c58dfdf767d52e1
Magdaline <mamoakohene@gmail.com>
2022-11-08 16:13:16 +0000

    Class test 2 ex. 1

--- spec/counter_spec.rb
+++ spec/counter_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Counter do
     result = counter.report
     expect(result).to eq "Counted to 0 so far."
   end
+
+  it "adds to the count" do
+    counter = Counter.new
+    counter.add(5)
+    expect(counter.report).to eq "Counted to 5 so far."
+  end
 end 
