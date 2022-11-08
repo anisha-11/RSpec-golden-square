c2e9319ee076f0d9bbe895b02a0f2b92d84b8d1c
Magdaline <mamoakohene@gmail.com>
2022-11-08 15:10:37 +0000

    challenge first test

---
+++ spec/report_length_spec.rb
@@ -0,0 +1,7 @@
+require "report_length"
+RSpec.describe "report_length method" do
+    it "returns a message of the string length" do
+        result = report_length("hello")
+        expect(result).to eq "This string was 5 characters long."
+    end
+end
