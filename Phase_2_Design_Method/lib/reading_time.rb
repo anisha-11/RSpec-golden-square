bf403c20c15e9ebaf9851aabc4157b875df14d07
Anisha Hirani <anisha.hirani11@gmail.com>
2022-11-09 14:45:19 +0000

    passed 2 tests

--- Phase_2_Design_Method/lib/reading_time.rb
+++ Phase_2_Design_Method/lib/reading_time.rb
@@ -1,2 +1,6 @@
 def reading_time(text)
+    wpm = 200.0
+    words = text.split.length
+    time = words / wpm 
+    return time.to_s + " minutes"
 end
--- Phase_2_Design_Method/spec/reading_times_spec.rb
+++ Phase_2_Design_Method/spec/reading_times_spec.rb
@@ -5,6 +5,13 @@ RSpec.describe "reading_time method" do
     arr = []
     400.times {arr.push("one")}
     text = arr.join(" ")
-    expect(reading_time(text)).to eq "2 minutes"
+    expect(reading_time(text)).to eq "2.0 minutes"
+  end
+
+  it "returns 2.5 minutes when input is 500 words" do
+    arr = []
+    500.times {arr.push("one")}
+    text = arr.join(" ")
+    expect(reading_time(text)).to eq "2.5 minutes"
   end
 end
