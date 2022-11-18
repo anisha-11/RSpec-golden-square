bb393a048c79d876c6978febc3b2a9736350e541
Anisha Hirani <anisha.hirani11@gmail.com>
2022-11-18 10:52:07 +0000

    challenge 1 completed

---
+++ mocking-bites/01_unit_testing_parent_child/spec/task_list_spec.rb
@@ -0,0 +1,41 @@
+require "task_list"
+
+RSpec.describe TaskList do
+  it "initially has an empty list" do
+    task_list = TaskList.new
+    expect(task_list.all).to eq []
+  end
+
+  it "is initially not all complete" do
+    task_list = TaskList.new
+    expect(task_list.all_complete?).to eq false
+  end
+
+  # Unit test `#all` and `#all_complete?` behaviour
+  it "returns task list" do 
+    list = TaskList.new 
+    task_1 = double(:task)
+    task_2 = double(:task)
+    list.add(task_1)
+    list.add(task_2)
+    expect(list.all).to eq [task_1, task_2]
+  end 
+
+  it "returns true if completed" do 
+    list = TaskList.new 
+    task_1 = double(:task, complete?: true)
+    task_2 = double(:task, complete?: true)
+    list.add(task_1)
+    list.add(task_2)
+    expect(list.all_complete?).to eq true 
+  end 
+
+  it "returns false if not completed" do 
+    list = TaskList.new 
+    task_1 = double(:task, complete?: false)
+    task_2 = double(:task, complete?: true)
+    list.add(task_1)
+    list.add(task_2)
+    expect(list.all_complete?).to eq false
+  end 
+end
